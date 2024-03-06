4a6e5fae6f4e302396d09e033f86559eaa3a0668
BhavyaParekh <123828041+BhavyaParekh@users.noreply.github.com>
2024-03-07 01:20:20 +0530

    Delete FsdProject directory

--- FsdProject/src/main/java/com/example/FsdProject/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.FsdProject.repositories;
-
-import com.example.FsdProject.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User , Integer> {
-
-}
